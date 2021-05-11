e6006ab00eceefb7b3a1e99db4acf2568d433cd5
W-Cephei <vycanismajoriscsa@gmail.com>
2021-05-11 15:46:30 -0300

    console.error route not found

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
 	selector: 'app-not-found',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
 	styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent {
-	constructor() {}
+	constructor(private router: Router) {
+		console.error(this.router.url)
+	}
 }
