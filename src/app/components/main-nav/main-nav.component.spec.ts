5897dcec1d7f0308e87febb86a97406fa8cee8fb
Edward McKnight <eddy@em-creations.co.uk>
2021-05-26 18:05:42 +0100

    Update: main-nav component brought up to 100% test coverage.

--- src/app/components/main-nav/main-nav.component.spec.ts
+++ src/app/components/main-nav/main-nav.component.spec.ts
@@ -1,5 +1,5 @@
 import { LayoutModule } from '@angular/cdk/layout';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -10,10 +10,20 @@ import { MainNavComponent } from './main-nav.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import {MatMenuModule} from '@angular/material/menu';
+import { UserService } from '../../services/user.service';
+import {Router} from "@angular/router";
+import { environment } from 'src/environments/environment';
 
+/**
+ * main-nav component unit tests
+ * @author StatusRed
+ * @since 2021
+ */
 describe('MainNavComponent', () => {
 	let component: MainNavComponent;
 	let fixture: ComponentFixture<MainNavComponent>;
+	let userService: UserService;
+	let router: Router;
 
 	beforeEach(
 		waitForAsync(() => {
@@ -33,13 +43,63 @@ describe('MainNavComponent', () => {
 		})
 	);
 
-	beforeEach(() => {
+	beforeEach(async(inject([UserService, Router], (s: UserService, r: Router) => {
+		userService = s;
+		router = r;
 		fixture = TestBed.createComponent(MainNavComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
-	});
+	})));
 
 	it('should compile', () => {
 		expect(component).toBeTruthy();
+	});
+
+	describe('on logging out', () => {
+		it('should call UserService.logout', async(() => {
+			const logoutSpy = spyOn(userService, 'logout').and.stub();
+
+			component.logout();
+
+			expect(logoutSpy).toHaveBeenCalled();
+		}));
+	});
+
+	describe('on opening mission list', () => {
+		it('should navigate to mission-list', async(() => {
+			const navigateSpy = spyOn(router, 'navigate').and.stub();
+			const expectedRoute = ['mission-list'];
+
+			component.openMissionList();
+
+			expect(navigateSpy).toHaveBeenCalledWith(expectedRoute);
+		}));
+	});
+
+	describe('on opening upload form', () => {
+		it('should navigate to mission-upload', async(() => {
+			const navigateSpy = spyOn(router, 'navigate').and.stub();
+			const expectedRoute = ['mission-upload'];
+
+			component.openUploadForm();
+
+			expect(navigateSpy).toHaveBeenCalledWith(expectedRoute);
+		}));
+	});
+
+	describe('on getting discord link', () => {
+		it('should return correct links', () => {
+			const expectedProdLink = 'https://discord.com/api/oauth2/authorize?client_id=731266255306227813&redirect_uri=https%3A%2F%2Fglobalconflicts.net%2Fapi%2Fauth%2Fcallback&response_type=code&scope=identify%20guilds';
+			const expectedNonProdLink = 'https://discord.com/api/oauth2/authorize?client_id=731266255306227813&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fapi%2Fauth%2Fcallback&response_type=code&scope=identify%20guilds';
+
+			environment.production = true;
+			const actualLinkProd = component.getDiscordHref();
+
+			environment.production = false;
+			const actualLinkNonProd = component.getDiscordHref();
+
+			expect(actualLinkProd).toEqual(expectedProdLink);
+			expect(actualLinkNonProd).toEqual(expectedNonProdLink);
+		});
 	});
 });
